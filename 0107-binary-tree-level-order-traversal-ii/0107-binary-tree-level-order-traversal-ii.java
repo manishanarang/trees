4423a18ade81182c060d2906629c0bd60db373e5
manishanarang <160212455+manishanarang@users.noreply.github.com>
2024-03-30 14:20:55 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (5.32%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -15,40 +15,27 @@
  */
 class Solution {
     List<List<Integer>> result = new ArrayList<>();
-    List<Integer> levelNodes = new ArrayList<>();
 
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        
-        for(int level = height(root); level>0; level--){
-            levelNodes = new ArrayList<>();
-            util(root, 1, level);
-            result.add(levelNodes);
-        }
+        util(root, 0);
+        Collections.reverse(result);
         return result;
     }
     
-    public void util(TreeNode root, int i, int level){
+    public void util(TreeNode root, int level){
         if(root == null){
             return;
         }
         
-        util(root.left, i+1, level);
-        util(root.right, i+1, level);
-        
-        if(i == level){
-            levelNodes.add(root.val);        
+        if(result.size() == level){
+            result.add(new ArrayList<>());
         }
+
+        result.get(level).add(root.val);
         
+        util(root.left, level+1);
+        util(root.right, level+1);         
     }
     
-    int height(TreeNode root){
-        if(root == null){
-            return 0;
-        }
-        int l = height(root.left);
-        int r = height(root.right);
-        
-        return 1 + Math.max(l,r);
-    }
     
 }
